31c45c9b63a2fb64574984cbda0beb43f97e45c1
Sateesh Kumar <sateesh2499@gmail.com>
2019-03-11 13:13:45 +0530

    1. Fixed schedule updation issue 2. Added support for adding multiple slots 3. Added validations for inputs 4. Updated styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { StudioService } from '../shared/studio.service';
 
@@ -8,13 +8,26 @@ import { StudioService } from '../shared/studio.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChildren('inputField') inputField: any;
+  @ViewChildren('startTimeField') startTimeField: any;
+  @ViewChildren('endTimeField') endTimeField: any;
+  
   studioForm: FormGroup;
-  slotsForm: FormGroup;
-  studioDetails: any;
-  searchSubmitted: boolean;
-  noSchedules: boolean;
-  displayError: boolean;
-  showSlotForm: boolean;
+  slot = {
+    startTime: '',
+    endTime: '',
+    faculty:  '',
+    assignerName:  ''
+  };
+  slotsList = [];
+  studioNotExists: boolean;
+  existingStudio: boolean;
+  formSubmitted: boolean;
+  successMsg: string = '';
+  errorMsg: string = '';
+  scheduleId: string = '';
+  datePattern = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+  timePattern = /^$|^(([01][0-9])|(2[0-3])):[0-5][0-9]$/;
 
   constructor(
     private studioService: StudioService
@@ -25,91 +38,134 @@ export class HomeComponent implements OnInit {
       studioName: new FormControl(''),
       localDate: new FormControl('')
     });
-
-    this.slotsForm = new FormGroup({
-      startTime: new FormControl(''),
-      endTime: new FormControl(''),
-      faculty:  new FormControl(''),
-      assignerName:  new FormControl(''),
-    });
   }
 
+  // add a time slot
+  addSlot(){
+    this.slotsList.push(JSON.parse(JSON.stringify(this.slot)));
+    this.studioNotExists = false;
+    console.log('this.slotsList', this.slotsList)
+  }
+
+  // remove a time slot
+  removeSlot(index){
+    this.slotsList.splice(index,1);
+  }
+
+  // save or update schedule
+  save(update:boolean){
+    console.log(this.inputField.toArray());
+    this.formSubmitted = true;
+    let valid = true;
+    this.inputField.toArray().forEach(item => {
+      if(!item.valid){
+        valid = false;
+        return
+      }
+    });
+    this.startTimeField.toArray().forEach(item => {
+      if(!item.valid){
+        valid = false;
+        return
+      }
+    });
+    this.endTimeField.toArray().forEach(item => {
+      if(!item.valid){
+        valid = false;
+        return
+      }
+    });
+
+    // proceed with calling apis if both forms are valid
+    if(valid && this.studioForm.valid){
+      this.formSubmitted = false;
+      let req = {
+        date: this.studioForm.value.localDate,
+        studioName: this.studioForm.value.studioName,
+        scheduleId: this.scheduleId,
+        studioScheduleSlotList: this.slotsList
+      }
+      if (update) {
+        //update
+        req['studioScheduleId'] = this.scheduleId;
+        console.log('update request', req);
+        this.studioService.updateStudio(req).subscribe((response:any) => {
+          console.log(response);
+          if(response && response.responseMsg == 'Schedule successfully updated'){
+            this.successMsg = response.responseMsg;
+          }else{
+            this.errorMsg = 'Could not update Schedule. Try again.';
+          }
+          this.clearMessages();
+        },
+        error => {
+          this.errorMsg = 'An error occured. Try again.';
+          this.clearMessages();
+        })
+      } else {
+        //save
+        this.studioService.saveStudio(req).subscribe((response:any) => {
+          console.log(response);
+          if(response && response.responseMsg == 'Schedule successfully created'){
+            this.successMsg = response.responseMsg;
+          }else{
+            this.errorMsg = 'Could not update Schedule. Try again.';
+          }
+          this.clearMessages();
+        },
+        error => {
+          this.errorMsg = 'An error occured. Try again.';
+          this.clearMessages();
+        })
+      }
+    }
+  }
+
+  // serach studio with studio name and date
   search(form: FormGroup){
-    this.displayError = false;
-    this.noSchedules = false;
-    this.showSlotForm = false;
     console.log('Valid?', form.valid);
+    this.formSubmitted = true;    
     if(form.valid){
+      this.formSubmitted = false;
       this.studioService.getStudioData(form.value).subscribe((response: any) => {
         console.log(response);
-        this.searchSubmitted = true;
         if(response){
-          this.noSchedules = false;
-          this.studioDetails = response;          
-          const formData: any = response.studioScheduleSlotList[0];
-          delete formData.studioScheduleSlotId
-          this.slotsForm.setValue(formData);
+          this.studioNotExists = false;
+          this.existingStudio = true;
+          this.scheduleId = response.studioScheduleId;
+          if(response.studioScheduleSlotList && response.studioScheduleSlotList.length){
+            this.slotsList = response.studioScheduleSlotList;
+          }         
         }else{
-          this.noSchedules = true;
+          this.studioNotExists = true;
+          this.existingStudio = false;
         }
       },
       error => {
-        console.log('error', error);
-        this.displayError = true;
+        this.errorMsg = 'An error occured. Try again.';
+        this.clearMessages();
       })
-    }
-    
+    }    
   }
 
-  displaySlot(){
-    this.showSlotForm = true;
+  // clear messages and reset form
+  clearMessages(){  
+    this.slotsList = [];
+    this.studioForm.reset();
+    this.studioNotExists = false;
+    this.existingStudio = false;
+    this.formSubmitted = false;
+    this.scheduleId = ''; 
+    setTimeout(() => {      
+      this.successMsg = '';
+      this.errorMsg = '';      
+    }, 5000);
   }
 
-  createStudio(studioForm: FormGroup, slotsForm: FormGroup, create:boolean){
-    if(studioForm.valid && slotsForm.valid){
-      const req = {
-        "date": studioForm.value.localDate,
-        "studioName": studioForm.value.studioName,
-        "studioScheduleSlotList": [
-          slotsForm.value
-        ]
-    }
-    if(!create){
-      this.studioService.updateStudio(req).subscribe((response:any) => {
-        console.log(response);
-        if(response && response.responseMsg == 'Schedule successfully updated'){
-          alert(response.responseMsg)
-        }else{
-          alert('Could not update Schedule');
-        }
-      },
-      error => {
-        console.log('error', error);
-        this.displayError = true;
-      })
-    }else{
-      this.studioService.saveStudio(req).subscribe((response:any) => {
-        console.log(response);
-        if(response && response.responseMsg == 'Schedule successfully created'){
-          alert(response.responseMsg)
-        }else{
-          alert('Could not create Schedule');
-        }
-      },
-      error => {
-        console.log('error', error);
-        this.displayError = true;
-      })
-    }
-      
-    }
-  }
-
+  // reset studio form
   reset(){
     this.studioForm.reset();
-    this.slotsForm.reset();
-    this.studioDetails = '';
-    this.showSlotForm = false;
-    this.displayError = false;
   }
+
+// end of component
 }
--- src/app/shared/studio.service.ts
+++ src/app/shared/studio.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Headers, RequestOptions } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class StudioService {
   baseUrl = 'http://13.232.30.248:8081/schedule';
+  headers = new Headers();
+  options: any;
+
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+    this.headers.append('Content-Type', 'application/json');
+    this.options = new RequestOptions({ headers: this.headers });
+   }
 
+  // check studio schedule 
   getStudioData(reqParams){
-    return this.http.post(this.baseUrl + '/check', reqParams);
+    return this.http.post(this.baseUrl + '/check', reqParams, this.options);
   }
 
+  // save studio schedule 
   saveStudio(reqData){
-    return this.http.post(this.baseUrl, reqData);
+    return this.http.post(this.baseUrl, reqData, this.options);
   }
 
+  // update studio schedule
   updateStudio(reqData){
-    return this.http.put(this.baseUrl, reqData);
+    return this.http.put(this.baseUrl, reqData, this.options);
   }
 }
